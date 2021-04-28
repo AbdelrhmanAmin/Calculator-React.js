b07a6aadb35fd317636d86f3691af22bbb02be20
AbdelrhmanAmin <maxhd213@gmail.com>
2021-04-28 20:23:56 +0200

    Refactor app

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,31 @@
 /* eslint-disable react/no-unused-state */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const App = () => {
+  const [data, setData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleClick = (btn) => {
-    const data = this.state;
+  const handleClick = (btn) => {
     const result = calculate(data, btn);
-    this.setState(result);
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div>
-        <h1>
-          Calculator
-        </h1>
-        <Display total={total} opt={operation} next={next} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </div>
-    );
-  }
-}
+    setData(result);
+  };
+  const { total, next, operation } = data;
+  return (
+    <div>
+      <h1>
+        Calculator
+      </h1>
+      <Display total={total} opt={operation} next={next} />
+      <ButtonPanel clickHandler={handleClick} />
+    </div>
+  );
+};
 
 export default App;
